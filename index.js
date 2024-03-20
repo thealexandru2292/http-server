892fe379a062b646e9d174369607b52c2d489291
Alexandru <thealexandru2292@gmail.com>
2024-03-20 20:55:54 +0200

    HTTP APIs, Routing, Parameterized URLs

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const http = require('http');
 const PORT = 3000;
 
-const server = http.createServer((req, resp) => {
+const server = http.createServer();
+
+const friends = [
+    {
+        id: 0,
+        name: 'Nikola Tesla',
+    },
+    {
+        id: 1,
+        name: 'Sir Isaac Newtor',
+    },
+    {
+        id: 2,
+        name: 'Albert Einstein'
+    }
+]
+
+server.on('request', (req, resp) => {
+    const items = req.url.split('/');
+
 //stream - a flow of data that is flowing    
 //req - is a readable stream
 //resp - is a writable stream     
 
-    if(req.url === '/friends')
+    if(items[1] === 'friends')
     {
-
        /*  resp.writeHead(200, {
             'Content-Type' : 'application/json',
         });
@@ -17,12 +35,18 @@ const server = http.createServer((req, resp) => {
         resp.statusCode = 200;
         resp.setHeader('Content-Type', 'application/json');
 
-        resp.end(JSON.stringify({
-            id:1,
-            name: 'Sir Isaac Newton!'
-        }));
+        if(items.length === 3)
+        {
+            const friendIndex = Number(items[2]); //from text to numnber + in front of variable OR use Number(items[2]);
+            resp.end(JSON.stringify(friends[friendIndex]));
+        }
+        else
+        {
+            resp.end(JSON.stringify(friends));
+        }
+        
     } 
-    else if (req.url === '/messages') 
+    else if (items[1] === 'messages') 
     { 
         //resp.statusCode = 200; by default is 200
         resp.setHeader('Content-Type', 'text/html');
